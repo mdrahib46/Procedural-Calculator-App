32aef6666d6c079836892d03cdb851c15adef2e0
mamunfarazi142 <mfmamun333@gmail.com>
2025-12-24 00:04:08 +0600

    Procedural Calculator App with 2 numbers calculation

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,24 +9,76 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _output = '0';
+  String _input = '0';
+  String _operator = '';
+  double num1 = 0;
+  double num2 = 0;
+
   @override
   Widget build(BuildContext context) {
+
+
+    void onBtnPress(String value) {
+
+      setState(() {
+        print('Pressed Button $value');
+
+        if (value == "C") {
+          _output = '0';
+          _input = '0';
+          _operator = '';
+          num1 = 0;
+          num2 = 0;
+        } else if (value == "=") {
+          num2 = double.parse(_input);
+
+          if (_operator == "+") {
+            _output = (num1 + num2).toString();
+          } else if (_operator == '-') {
+            _output = (num1 - num2).toString();
+          } else if (_operator == "x") {
+            _output = (num1 * num2).toString();
+          } else if (_operator == '÷') {
+            _output = (num2 != 0) ? (num1 / num2).toString() : 'Division Error';
+          }
+
+          _input = _output;
+        } else if (['+', '-', 'x', '÷'].contains(value)) {
+          num1 = double.parse(_input);
+          _operator = value;
+          _input = '0';
+        } else {
+          if(_input == '0'){
+            _input = value;
+          }else{
+            _input += value;
+          }
+          _output = _input;
+
+        }
+      });
+    }
+
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
           children: [
             Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
               alignment: .bottomRight,
               child: Column(
                 children: [
                   Text(
-                    '10+50',
-                    style: TextStyle(color: Colors.white, fontSize: 24),
+                    '$num1 $_operator $num2',
+                    style: TextStyle(color: Colors.white54, fontSize: 22),
                   ),
                   SizedBox(height: 20),
                   Text(
-                    '60',
+                    _output,
                     style: TextStyle(
                       fontSize: 50,
                       color: Colors.white,
@@ -36,36 +88,37 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
             ),
+            const Spacer(),
             Row(
               children: [
-                BuildButton(onTap: (){}, text: "7"),
-                BuildButton(onTap: (){}, text: "8"),
-                BuildButton(onTap: (){}, text: "9"),
-                BuildButton(onTap: (){}, text: "÷", color: Colors.orange,),
+                BuildButton(onTap: ()=> onBtnPress('7'), text: "7"),
+                BuildButton(onTap: ()=> onBtnPress('8'), text: "8"),
+                BuildButton(onTap: ()=> onBtnPress('9'), text: "9"),
+                BuildButton(onTap: ()=> onBtnPress('÷'), text: "÷", color: Colors.orange,),
               ],
             ),
             Row(
               children: [
-                BuildButton(onTap: (){}, text: "4"),
-                BuildButton(onTap: (){}, text: "5"),
-                BuildButton(onTap: (){}, text: "6"),
-                BuildButton(onTap: (){}, text: "x", color: Colors.orange,),
+                BuildButton(onTap: ()=> onBtnPress('4'), text: "4"),
+                BuildButton(onTap: ()=> onBtnPress('5'), text: "5"),
+                BuildButton(onTap: ()=> onBtnPress('6'), text: "6"),
+                BuildButton(onTap: ()=> onBtnPress('x'), text: "x", color: Colors.orange,),
               ],
             ),
             Row(
               children: [
-                BuildButton(onTap: (){}, text: "1"),
-                BuildButton(onTap: (){}, text: "2"),
-                BuildButton(onTap: (){}, text: "3"),
-                BuildButton(onTap: (){}, text: "-", color: Colors.orange,),
+                BuildButton(onTap: ()=> onBtnPress('1'), text: "1"),
+                BuildButton(onTap: ()=> onBtnPress('2'), text: "2"),
+                BuildButton(onTap: ()=> onBtnPress('3'), text: "3"),
+                BuildButton(onTap: ()=> onBtnPress('-'), text: "-", color: Colors.orange,),
               ],
             ),
             Row(
               children: [
-                BuildButton(onTap: (){}, text: "C"),
-                BuildButton(onTap: (){}, text: "0"),
-                BuildButton(onTap: (){}, text: "="),
-                BuildButton(onTap: (){}, text: "+", color: Colors.orange,),
+                BuildButton(onTap: ()=> onBtnPress('C'), text: "C"),
+                BuildButton(onTap: ()=> onBtnPress('0'), text: "0"),
+                BuildButton(onTap: ()=> onBtnPress('='), text: "="),
+                BuildButton(onTap: ()=> onBtnPress('+'), text: "+", color: Colors.orange,),
               ],
             ),
           ],
